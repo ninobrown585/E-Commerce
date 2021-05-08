b015ded52fbdba11dc2297e0d4a3791cadf882de
NinoB <adrian.brown1@g.austincc.edu>
2021-05-08 17:30:10 -0500

    updated commit tres

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,18 +3,38 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/',  async(req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const allCategory =  await Category.findAll();
+    res.status(200).json(allCategory);
+  }
+  catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    Category.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [Product],
+    })
+    .then((answer) => res.json(answer))
+  }
+  catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  
 });
 
 router.put('/:id', (req, res) => {
